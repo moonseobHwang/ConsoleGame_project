6f3f36ff37084fdf653694aa3998a267ddb791f4
sanghoon96 <sanghoon96@naver.com>
2020-11-19 13:48:45 +0900

    11.19.목 화면띄우기 중간점검

--- Storyline/TestFirstScreen.cpp
+++ Storyline/TestFirstScreen.cpp
@@ -8,7 +8,16 @@ int main()
     WINDOW *w;
     w = initscr();
     using namespace std;                        // start main
+    timeout(1000);
     int max_y = LINES - 1, max_x = COLS - COLS, move_x = 0, move_y = 0; // define in curses.h
-    mvaddstr(max_y - 1, COLS / 3, "Title : JoJo's Bizarre Adventure ");
-    getch();
+    int ch = 'y';
+    while ((ch != 'q') && (ch != 'Q'))
+    {
+        mvaddstr(max_y - 1, COLS / 3, "                                 ");
+        max_y = max_y - 2 ;
+        mvaddstr(max_y - 1, COLS / 3, "Title : JoJo's Bizarre Adventure ");
+        ch = getch();
+    }
+    endwin();
+    return 0;
 }
