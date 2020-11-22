3f7f8d1e0372ec466fa9a1cfe4e83a4e79fac75e
sanghoon96 <sanghoon96@naver.com>
2020-11-22 14:48:05 +0900

    game start screen

--- GameScreen/game.c
+++ GameScreen/game.c
@@ -1,4 +1,14 @@
 #include "game.h" 
+#ifndef DWORD
+#define WINAPI
+typedef unsigned long DWORD;
+typedef short WCHAR;
+typedef void * HANDLE;
+#define MAX_PATH    PATH_MAX
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+typedef unsigned int BOOL;
+#endif
 
 int keyControl(){
 	char temp = getch();
